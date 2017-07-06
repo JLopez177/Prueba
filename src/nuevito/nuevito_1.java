0ff5d60a9bafbeb69d5f06db46a9cd19e40a5091
PC8 <PC8@laboratorio_pc8>
2017-07-06 09:16:52 -0600

    agregando la clase Mecanicos

---
+++ src/nuevito/nuevito_1.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nuevito;
+
+/**
+ *
+ * @author PC8
+ */
+public class nuevito_1 {
+    
+}
