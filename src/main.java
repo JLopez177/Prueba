fe89a1a6c5f3ee1e86b063e4abd020dbc424d621
fledd <leon.22010@hotmail.com>
2017-07-06 09:06:03 -0600

    Subida del proyecto por primera vez

---
+++ src/main.java
@@ -0,0 +1,17 @@
+
+public class main {
+
+    public static void main(String[] args) {
+        
+        for(int i=0;i<101;i++)
+        {
+            System.out.println(i+2);
+            
+        }
+        
+        
+        
+        
+    }
+    
+}
